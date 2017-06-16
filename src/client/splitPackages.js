625a2a2870a17164a7784b674f19e54556e4579f
Vikari <jukka.vihtakari@gmail.com>
2017-06-16 18:19:59 +0300

    Removed useless vars from splitPackage

--- src/client/splitPackages.js
+++ src/client/splitPackages.js
@@ -1,11 +1,9 @@
 // Splits given array to two even long arrays
 export default packages => {
   if (packages) {
-    let deck1;
-    let deck2;
     return [
-      (deck1 = packages.slice(0, packages.length / 2)),
-      (deck2 = packages.slice(packages.length / 2))
+      packages.slice(0, packages.length / 2),
+      packages.slice(packages.length / 2)
     ];
   }
 };
